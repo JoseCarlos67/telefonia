1d0ba8172538e119e6aeecd1212afc1ca7fdb72f
José Carlos <jose_carlos676@outlook.com>
2022-09-22 07:43:38 -0300

    first commit

---
+++ src/main/java/ifsuldeminas/telefonia/ifsuldeminas/IfsuldeminasApplication.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.telefonia.ifsuldeminas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfsuldeminasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfsuldeminasApplication.class, args);
+	}
+
+}
